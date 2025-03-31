03aab51fc2f1f39e76ddae6731f19051e90b43bc
Romeoisl <167251263+Romeoisl@users.noreply.github.com>
2025-03-31 22:26:20 +0100

    Update login.js

--- bot/login/login.js
+++ bot/login/login.js
@@ -1,5 +1,5 @@
 // set bash title
-process.stdout.write("\x1b]2;Goat Bot V2 - Made by NTKhang Fixed by Team Calyx\x1b\x5c");
+process.stdout.write("\x1b]2;Goat Bot V2 - Made by NTKhang Fixed by Team MateoKafla\x1b\x5c");
 const defaultRequire = require;
 
 function decode(text) {
@@ -15,7 +15,7 @@ const path = defaultRequire("path");
 const readline = defaultRequire("readline");
 const fs = defaultRequire("fs-extra");
 const toptp = defaultRequire("totp-generator");
-const login = defaultRequire(`${process.cwd()}/fb-chat-api`);
+const login = defaultRequire(`${process.cwd()}/facebook-chat-api`);
 const qr = new (defaultRequire("qrcode-reader"));
 const Canvas = defaultRequire("canvas");
 const https = defaultRequire("https");
